4e717e8332217b9aaa070268a7d7693410633768
leonardoproietti <proiettileonardo@hotmail.it>
2024-06-22 17:45:18 +0700

    add super mega ultra special effects

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -11,5 +11,33 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrolled = window.scrollY;
+    const headerContainer = document.querySelector('.header-container') as HTMLElement;
+    const centeredText = document.querySelector('.centered-text') as HTMLElement;
+    const textBelow = document.querySelector('.text-below') as HTMLElement;
 
+    const fadeStart = 100;
+    const fadeEnd = 400;
+
+    if (scrolled <= fadeEnd) {
+      const opacity = 1 - ((scrolled - fadeStart) / (fadeEnd - fadeStart));
+      const scale = 1 - ((scrolled - fadeStart) / (fadeEnd - fadeStart)) * 0.5;
+
+      centeredText.style.opacity = opacity.toString();
+      centeredText.style.transform = `scale(${scale})`;
+
+      textBelow.style.opacity = opacity.toString();
+      textBelow.style.transform = `scale(${scale})`;
+    } else {
+      centeredText.style.opacity = '0';
+      centeredText.style.transform = 'scale(0.5)';
+
+      textBelow.style.opacity = '0';
+      textBelow.style.transform = 'scale(0.5)';
+    }
+
+    headerContainer.style.transform = `translateY(${scrolled * 0.1}px)`;
+  }
 }
